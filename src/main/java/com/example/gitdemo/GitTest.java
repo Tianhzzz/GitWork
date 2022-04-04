c0fdc81e46aae5b22573e381145da50dc38cc82a
bth <1275483732@qq.com>
2022-04-04 18:15:00 +0800

    second commit

--- src/main/java/com/example/gitdemo/GitTest.java
+++ src/main/java/com/example/gitdemo/GitTest.java
@@ -6,5 +6,6 @@ package com.example.gitdemo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
